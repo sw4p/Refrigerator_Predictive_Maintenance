39e3034fc6e177f285d1c67e11c120228d56bd34
Swapnil Verma <usav.swapnil@gmail.com>
2022-05-10 00:22:01 +0100

    Simplified and refactored the dataset_collector code

--- Dataset_Collector/Dataset_Collector.ino
+++ Dataset_Collector/Dataset_Collector.ino
@@ -1,19 +1,19 @@
 /*
-  HTS221 - Read Sensors
+  Refrigerator Predictive Maintenance - Data Collector
 
-  This example reads data from the on-board HTS221 sensor of the
-  Nano 33 BLE Sense and prints the temperature and humidity sensor
-  values to the Serial Monitor once a second.
+  This code reads various sensor values of the Arduino BLE Sense
+  and send it to a BLE peripheral device in a CSV format.
 
   The circuit:
   - Arduino Nano 33 BLE Sense
 
-  This example code is in the public domain.
+  Creator:
+  - Swapnil Verma <usav.swapnil[at]gmail.com>
 */
 
-#include <Arduino_HTS221.h>
-#include <PDM.h>
-#include <Arduino_APDS9960.h>
+#include <Arduino_HTS221.h> ///< Temperature and Humidity sensor
+#include <PDM.h>  ///< To read digital microphone output
+#include <Arduino_APDS9960.h> ///< Proximity and Gesture detection sensor
 #include <ArduinoBLE.h>
 
 // default number of output channels
@@ -28,9 +28,21 @@ short sampleBuffer[512];
 // Number of audio samples read
 volatile int samplesRead;
 
+// BLE constants
 const char* deviceServiceUuid = "19b10000-e8f2-537e-4f6c-d104768a1214";
 const char* deviceServiceCharacteristicUuid = "19b10001-e8f2-537e-4f6c-d104768a1214";
 const char* stringCharacteristicUuid = "1A3AC131-31EF-758B-BC51-54A61958EF82";
+
+// Set to false if not in Debug mode
+static const bool debug_mode = true;
+
+void print(const String information, bool new_line = true)
+{
+  if (!debug_mode) return;
+  
+  if (new_line) Serial.println(information);
+  else Serial.print(information);
+}
 
 void setup() {
 
@@ -39,16 +51,21 @@ void setup() {
   pinMode(LEDG, OUTPUT);
   pinMode(LEDB, OUTPUT);
   
-  digitalWrite(LEDR, HIGH);               // will turn the LED off
-  digitalWrite(LEDG, HIGH);                // will turn the LED off
-  digitalWrite(LEDB, HIGH);               // will turn the LED off  
+  digitalWrite(LEDR, HIGH);   // will turn the LED off
+  digitalWrite(LEDG, HIGH);   // will turn the LED off
+  digitalWrite(LEDB, HIGH);   // will turn the LED off  
   
-  Serial.begin(9600);
-  while (!Serial);
+  // Start Serial communication in debug mode
+  if (debug_mode)
+  {
+    Serial.begin(9600);
+    while (!Serial) blinkLED(LEDR, 1);
+  }
 
   // Configure the data receive callback
   PDM.onReceive(onPDMdata);
 
+  // Initialise humidity and temperature sensor
   if (!HTS.begin()) {
     Serial.println("Failed to initialize humidity temperature sensor!");
     while (1) blinkLED(LEDR, 1);
@@ -59,30 +76,27 @@ void setup() {
     while (1) blinkLED(LEDR, 1);
   }
 
+  // Initialise gesture and proximity detection sensor
   if (!APDS.begin()) {
     Serial.println("Error initializing APDS9960 sensor!");
     while (1) blinkLED(LEDR, 1);
   }
 
-  // BLE.begin();
-  // BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
-
+  // Initialise BLE communication
   if (!BLE.begin()) {
-    Serial.println("* Starting Bluetooth® Low Energy module failed!");
-    while (1);
+    Serial.println("Starting Bluetooth® Low Energy module failed!");
+    while (1) blinkLED(LEDR, 1);
   }
   
-  BLE.setLocalName("Data Collector BLE Sense"); 
+  BLE.setLocalName("BLESense_Data_Collector"); 
   BLE.advertise();
 
-  blinkLED(LEDG, 3);
+  blinkLED(LEDG, 3);  ///< Indicate that everything is fine
 }
 
 void loop() {
-  // check if a peripheral has been discovered
-  // BLEDevice peripheral = BLE.available();
   BLEDevice peripheral;
-  Serial.println("- Discovering peripheral device...");
+  Serial.println("Discovering peripheral device...");
 
   do
   {
@@ -100,17 +114,12 @@ void loop() {
     Serial.print(peripheral.advertisedServiceUuid());
     Serial.println();
 
-//    if (peripheral.localName() != "LED") {
-//      return;
-//    }
-
     // stop scanning
     BLE.stopScan();
 
     connectPeripheral(peripheral);
 
     // peripheral disconnected, start scanning again
-    // BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
   }
 }
 
@@ -137,25 +146,25 @@ void connectPeripheral(BLEDevice peripheral)
   }
 
   // retrieve the LED characteristic
-  BLECharacteristic ledCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
+  // BLECharacteristic ledCharacteristic = peripheral.characteristic(deviceServiceCharacteristicUuid);
   BLECharacteristic stringCharacteristic = peripheral.characteristic(stringCharacteristicUuid);
 
-  if (!ledCharacteristic || !stringCharacteristic) {
-    Serial.println("* Peripheral device does not have characteristic!");
+  if (/*!ledCharacteristic ||*/ !stringCharacteristic) {
+    Serial.println("Peripheral device does not have required characteristic!");
     peripheral.disconnect();
     return;
-  } else if (!ledCharacteristic.canWrite() || !stringCharacteristic.canWrite()) {
-    Serial.println("* Peripheral does not have a writable characteristic!");
+  } else if (/*!ledCharacteristic.canWrite() || */!stringCharacteristic.canWrite()) {
+    Serial.println("Peripheral does not have a writable characteristic!");
     peripheral.disconnect();
     return;
   }
 
   char val[5] = "Hiya";
   while (peripheral.connected()) {
-//    sendData(stringCharacteristic);
-    if (stringCharacteristic.writeValue(val)) Serial.println("Sent");
-    else Serial.println("Not Sent");
-    delay(2000);
+    sendData(stringCharacteristic);
+    // if (stringCharacteristic.writeValue(val)) Serial.println("Sent");
+    // else Serial.println("Not Sent");
+    // delay(2000);
   }
 
   Serial.println("Peripheral disconnected");
@@ -163,20 +172,23 @@ void connectPeripheral(BLEDevice peripheral)
 
 void sendData(BLECharacteristic stringCharacteristic)
 {
+  String sensorReading;
   // read all the sensor values
   float temperature = HTS.readTemperature();
   float humidity    = HTS.readHumidity();
 
+  sensorReading += "T" + String(temperature, 2) + ",H" + String(humidity, 2) + ",";
   // print each of the sensor values
-  // Serial.print("Temperature = ");
-  // Serial.print(temperature);
-  // Serial.println(" °C");
-
-  // Serial.print("Humidity    = ");
-  // Serial.print(humidity);
-  // Serial.println(" %");
+//  Serial.print("Temperature = ");
+//  Serial.print(temperature);
+//  Serial.println(" °C");
+//
+//  Serial.print("Humidity    = ");
+//  Serial.print(humidity);
+//  Serial.println(" %");
   
   // Wait for samples to be read
+  /*
   if (samplesRead) {    
     // Print samples to the serial monitor or plotter
     for (int i = 0; i < samplesRead; i++) {
@@ -198,23 +210,28 @@ void sendData(BLECharacteristic stringCharacteristic)
     // Clear the read count
     samplesRead = 0;
   }
+  */
 
   // Ambient light detection
-  int r, g, b, a;
-  char val[20];
   if (APDS.colorAvailable())
   {
+    int r, g, b, a;
     APDS.readColor(r, g, b, a);
+    Serial.print("Ambient Light = ");
     Serial.println(a);
-//    stringCharacteristic.writeValue(itoa(a, val, 10));
-    stringCharacteristic.writeValue("Hi");
+
+    // print("Ambient Light = " + String(a));
+
+    sensorReading += "A" + String(a);
   }
 
   // print an empty line
-  Serial.println();
+  Serial.print("Sending = ");
+  Serial.println(sensorReading);
+  stringCharacteristic.writeValue(sensorReading.c_str());
 
   // wait 1 second to print again
-  delay(100);  
+  delay(1000);
 }
 
 /**
